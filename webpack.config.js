679753a420a10ecf2020ca73e1e4afb65597f939
Oguntuberu Nathan O <nateoguns.work@gmail.com>
2019-04-09 21:40:13 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/** */
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            { // Load JS
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            { // Load CSS
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { // Load Images
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000
+                        }
+                    },
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
