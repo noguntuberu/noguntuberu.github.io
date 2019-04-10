0deedaeb7f94d339c74d6b62a811cf4b3e86a57d
Oguntuberu Nathan O <nateoguns.work@gmail.com>
2019-04-10 11:30:02 +0100

    Added floating Menu component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import '@babel/polyfill'
+import '@babel/polyfill';
+import Header from './components/header/index';
+import Landing from './components/landing/index';
+import Work from './components/work/index';
+import Contact from './components/contact/index.js';
+import Footer from './components/footer/index.js';
+import FloatingMenu from './components/floating-btn/index';
+
+//
+const React = require('react'),
+      ReactDOM = require('react-dom');
+
+//
+class AppComponent extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header />
+                <Landing />
+                <div>
+                    <Work />
+                    <Contact />
+                </div>
+                <FloatingMenu />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+//
+window.addEventListener('load', () => {
+    const wrapper = document.getElementById('app-wrapper');
+    ReactDOM.render(<AppComponent />, wrapper);
+});
