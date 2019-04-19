e8a97ec7b0fcc1e683c772e26cb3be3e26ac4b82
Oguntuberu Nathan O <nateoguns.work@gmail.com>
2019-04-19 20:17:47 +0100

    Updated keywords in package.json

---
+++ tests/components/landing.test.js
@@ -0,0 +1,9 @@
+/** */
+const React = require('react');
+const LandingComponent = require('../../src/components/landing/index');
+import { shallow } from 'enzyme';
+
+//
+describe("LandingComponent Test", () => {
+    
+})
